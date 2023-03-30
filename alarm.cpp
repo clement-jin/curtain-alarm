e5f5383c4cf484f0644257517ac5b043446a1291
clement-jin <clement.yunzhuo.jin@gmail.com>
2023-03-30 16:18:50 +0100

    fixed 'motor not running' issue

--- alarm.cpp
+++ alarm.cpp
@@ -10,39 +10,44 @@ int no_buzzes = 2;
 int buzz_duration = 1000; // how long each buzz and each delay lasts
 
 // motor configuration
-int curtain_lift_duration = 15000;
+int curtain_lift_duration = 10000;
 
 
 // everything inside alarm() is called when the alarm goes off
 alarm() {
+  
+  sevseg.setChars("HEYO");
+  
   int buzz_count = 0;
   int motor_reference = millis();
   int buzzer_reference = millis();
   int buzzer_state = 0;
 
+  Serial.println("motor ON");
+  
   digitalWrite(transistor_pin, HIGH); // turn motor on
   
-  while (buzz_count <= 2*no_buzzes or digitalRead(transistor_pin) == 1) { // 2 times because buzz_count counts buzzes and delays
+  while (buzz_count <= 2*no_buzzes or (millis() - motor_reference < curtain_lift_duration)) { // 2 times because buzz_count counts buzzes and delays
+
+    sevseg.refreshDisplay();
+    
     if (buzz_count <= 2*no_buzzes and millis() - buzzer_reference >= buzz_duration) {
         buzz_count += 1;
         digitalWrite(buzzer_pin, buzz_count % 2);
         buzzer_reference = millis();
-        Serial.println(buzz_count);
+        Serial.println("Buzz times: " + String(buzz_count/2));
     }
 
     if (buzz_count == 2*no_buzzes) {
-      Serial.println("digital write to low");
+      Serial.println("turned buzzer off");
       buzz_count += 1; // make sure the buzz_count is now greater 
       digitalWrite(buzzer_pin, LOW);
     }
 
     if (millis() - motor_reference >= curtain_lift_duration) {
+      Serial.println("Motor turned OFF");
       digitalWrite(transistor_pin, LOW);
-      motor_reference = (2^15)-1; // largest value for int, so this code does not run again
     }
   }
-
-  digitalWrite(transistor_pin, LOW); // just in case the above code fails 
-  digitalWrite(buzzer_pin, LOW); // make sure the buzzer is off!!!
   
 }
